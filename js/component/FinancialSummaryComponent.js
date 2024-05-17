f262160713dfc274e291b72f57feee4a65e2f1df
Adam <48989841+atmostadam@users.noreply.github.com>
2024-05-17 13:18:03 -0400

    Grid

---
+++ js/component/FinancialSummaryComponent.js
@@ -0,0 +1,27 @@
+import { RectangleOutlineDrawing } from "../drawing/RectangleOutlineDrawing.js";
+import {
+    GAME_SCREEN_FINANCIALS_PERCENT_X,
+    GAME_SCREEN_FINANCIALS_PERCENT_Y,
+    GAME_SCREEN_FINANCIALS_PERCENT_W,
+    GAME_SCREEN_FINANCIALS_PERCENT_H,
+    GAME_SCREEN_FINANCIALS_PERCENT_BORDER,
+    GAME_SCREEN_FINANCIALS_COLOR
+} from "./../configuration/GameConfiguration.js";
+
+export class FinancialSummaryComponent {
+    constructor(context) {
+        this.context = context;
+    }
+
+    draw() {
+        new RectangleOutlineDrawing(
+            this.context,
+            GAME_SCREEN_FINANCIALS_PERCENT_X,
+            GAME_SCREEN_FINANCIALS_PERCENT_Y,
+            GAME_SCREEN_FINANCIALS_PERCENT_W,
+            GAME_SCREEN_FINANCIALS_PERCENT_H,
+            GAME_SCREEN_FINANCIALS_PERCENT_BORDER,
+            GAME_SCREEN_FINANCIALS_COLOR)
+            .draw();
+    }
+}
